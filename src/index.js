6bfc83d24c5725b02cb02c2c87061d560190a426
mgreif <m.greif@autohaus-royal.de>
2021-05-17 14:44:32 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log(1)
+console.log('starting...')
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const puppeteer = require('puppeteer-extra')
+
+puppeteer.use(StealthPlugin())
+
+puppeteer.launch({ headless: true }).then(async browser => {
+  console.log('Running crawler...')
+  const page = await browser.newPage()
+  const helper = usePuppeteer()
+  helper.initPage(page)
+
+  await page.goto('https://www.drivingcenter.de/fahrtrainings/fahrtrainings-motorrad/')
+  await page.waitForTimeout(5000)
+  const allTrainings = helper.getDataFromSelector('.am-tile-fahrtraining > li > h2')
+  console.log(allTrainings)
+  await browser.close()
+})
+
+function usePuppeteer () {
+  let page
+
+  const initPage = _page => {
+    page = _page
+  }
+
+  const getDataFromSelector = async (selector) => {
+    return await page.evaluate(async (selector) => {
+      return await new Promise(resolve => {
+        setTimeout(() => {
+          resolve(Array.from(document.querySelectorAll(selector)))
+        }, 1000)
+      })
+    })
+  }
+
+  return {
+    initPage,
+    getDataFromSelector
+  }
+}
