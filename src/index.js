e6452c4d53ade9417057a9b5e684fbbde4cc5e18
mgreif <m.greif@autohaus-royal.de>
2021-05-18 08:43:50 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -8,28 +8,36 @@ puppeteer.use(StealthPlugin())
 puppeteer.launch({ headless: true }).then(async browser => {
   console.log('Running crawler...')
   const page = await browser.newPage()
-  page.setViewport({ width: 1366, height: 768 })
   const helper = usePuppeteer()
+  const DELAY_HOMEPAGE = 2000
+  const DELAY_TRAINING_PAGE = 5000
+
+  page.setViewport({ width: 1366, height: 768 })
   helper.initPage(page)
-  helper.initBrowser(browser)
 
   await page.goto('https://www.drivingcenter.de/fahrtrainings/fahrtrainings-motorrad/')
-  await page.waitForTimeout(2000)
+  await page.waitForTimeout(DELAY_HOMEPAGE)
   const allTrainings = await helper.getAllTrainings()
   const allBookingPages = await helper.getAllBookingPages()
 
   console.log('all Training:', allTrainings)
   console.log('all Booking Pages:', allBookingPages)
 
-  const allPagesWithAppointments = await Promise.all(allBookingPages.map(helper.delayLoop(helper.getListOfAppointmentsForBookingPage, 5000)))
-  console.log(allPagesWithAppointments)
+  const allPagesWithAppointments = await Promise.all(
+    allBookingPages.map(
+      helper.delayLoop(
+        helper.getListOfAppointmentsForBookingPage,
+        DELAY_TRAINING_PAGE
+      )
+    )
+  )
 
   const allPagesWithAvailableAppointments = allPagesWithAppointments.map(page => {
     return page.filter(appointment => appointment.available > 0)
   }).filter(x => x.length)
 
   console.log('------------------- Available Trainings -------------------\n')
-  console.log(allPagesWithAvailableAppointments.forEach(page => {
+  allPagesWithAvailableAppointments.forEach(page => {
     console.log(`\n-------- ${page[0].training} --------\n`)
     page.forEach(appointment => {
       const {
@@ -43,37 +51,42 @@ puppeteer.launch({ headless: true }).then(async browser => {
       const _end = new Date(end).toDateString()
       console.log(`--> Begin: ${_begin} - End: ${_end} - Available: ${available} - ${price} - ${link}`)
     })
-  }))
+  })
 
   await browser.close()
 })
 
 function usePuppeteer () {
   let page
-  let browser
 
   const initPage = _page => {
     page = _page
   }
 
-  const initBrowser = _browser => {
-    browser = _browser
-  }
-
   const getAllTrainings = async () => {
+    const selector = '.am-tile-fahrtraining > li > h2'
+
     return await page.evaluate((sel) => {
       const results = [...document.querySelectorAll(sel)]
       return results.map(item => item.innerText)
-    }, '.am-tile-fahrtraining > li > h2')
+    }, selector)
   }
 
   const getAllBookingPages = async () => {
+    const selector = '.am-tile-fahrtraining > li:nth-child(5) > a'
+
     return await page.evaluate((sel) => {
       const results = [...document.querySelectorAll(sel)]
       return results.map(item => item.href)
-    }, '.am-tile-fahrtraining > li:nth-child(5) > a')
+    }, selector)
   }
 
+  // This function is used to map through an array of items,
+  // and execute a function one after another
+  /*
+  Example: await Promise.all(numbers.map(delayLoop((num) => num * num, 1000)))
+  Each second one items of the Array numbers gets squared
+  */
   function delayLoop (fn, delay) {
     return (name, i) => {
       return new Promise((resolve, reject) => {
@@ -86,7 +99,9 @@ function usePuppeteer () {
 
   const getListOfAppointmentsForBookingPage = async (bookingPage) => {
     const currentPage = await page.evaluate(() => document.location.href)
-    console.log('current Page: ', currentPage)
+    console.log('current page:', currentPage)
+    console.log('now crawling page:', bookingPage)
+
     await page.goto(bookingPage)
     const rows = await page.evaluate(() => {
       const rows = [...document.querySelectorAll('table > tbody > tr')]
@@ -129,7 +144,6 @@ function usePuppeteer () {
     getAllTrainings,
     getAllBookingPages,
     getListOfAppointmentsForBookingPage,
-    initBrowser,
     delayLoop
   }
 }
