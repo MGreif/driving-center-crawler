9addfe9cf206d9f2ac40ddb1abff2fee63df6bd4
mgreif <mika.greif@googlemail.com>
2022-03-30 21:36:22 +0200

    Add Nodemailer config. Add functionsality to send Email with generated HTML

---
+++ libs/nodemailer.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer')
+
+const sendMail = async (stringifiedHtml = "") => {
+  const {
+    NODEMAILER_HOST,
+    NODEMAILER_PORT,
+    NODEMAILER_SECURE,
+    NODEMAILER_USER,
+    NODEMAILER_PASSWORD,
+    NODEMAILER_RECIPIENTS,
+    NODEMAILER_SEND_MAILS
+  } = process.env
+
+  if (!Boolean(NODEMAILER_SEND_MAILS)) {
+    return
+  }
+
+  if (!NODEMAILER_HOST) {
+    throw new Error("No Mailserver settings configured")
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: NODEMAILER_HOST,
+    port: NODEMAILER_PORT,
+    secure: NODEMAILER_SECURE,
+    auth: {
+      user: NODEMAILER_USER,
+      pass: NODEMAILER_PASSWORD,
+    },
+  });
+
+    const info = await transporter.sendMail({
+      from: NODEMAILER_USER,
+      to: NODEMAILER_RECIPIENTS,
+      subject: "Drivingcenter Results ✔",
+      html: stringifiedHtml
+    });
+
+    console.log(`[X] EMAIL SENT TO ${NODEMAILER_RECIPIENTS} - ${info.messageId}`)
+
+  return info
+}
+
+module.exports = { sendMail }
